7406ba1dec2deff4fa169b6a0f52b5252f335de7
Nick Salloum <nick@callmenick.com>
2019-07-06 20:57:48 -0700

    adds more sections

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,21 @@
 import React, { Fragment } from 'react';
+import Cta from 'components/Cta';
 import Hero from './Hero';
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <Cta
+        heading="Fairly-sourced, consistent, & traceable beans"
+        description="In the first call to action, we should talk about our emphasis on traceability and consistency of our supply, with a link to read more"
+        action={{
+          text: 'Learn more',
+          href: '/about',
+        }}
+        appearance="primary"
+      />
+      <div>more stuff</div>
     </Fragment>
   );
 }
