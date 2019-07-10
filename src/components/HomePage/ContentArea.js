86921fd47d7aa080940cbaeb13dcc7ae51f41fbb
Nick Salloum <nick@callmenick.com>
2019-07-10 08:54:10 +0200

    adds new content area to home page

---
+++ src/components/HomePage/ContentArea.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Grid from 'components/Grid';
+import Content from 'components/Content';
+import Button from 'components/Button';
+import css from './ContentArea.css';
+
+function ContentArea() {
+  return (
+    <Grid padded>
+      <div className={css.imgWrapper}>
+        <img className={css.img} src="/static/img/placeholder/p2.svg" alt="" />
+      </div>
+      <div className={css.content}>
+        <Content>
+          <h2>At Ubergreen, we’re making it our mission to…</h2>
+          <p>
+            Here, we can talk just a little bit more in depth about our core
+            value props outlined above. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>
+            <Button
+              theme={{
+                appearance: 'primary',
+                display: 'inlineBlock',
+                size: 'md',
+              }}
+            >
+              Get in touch
+            </Button>
+          </p>
+        </Content>
+      </div>
+    </Grid>
+  );
+}
+
+export default ContentArea;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,21 +2,32 @@ import React, { Fragment } from 'react';
 import Cta from 'components/Cta';
 import Hero from './Hero';
 import Gallery from './Gallery';
+import ContentArea from './ContentArea';
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
       <Cta
+        appearance="primary"
         heading="Fairly-sourced, consistent, & traceable beans"
         description="In the first call to action, we should talk about our emphasis on traceability and consistency of our supply, with a link to read more"
         action={{
           text: 'Learn more',
           href: '/about',
         }}
-        appearance="primary"
       />
       <Gallery />
+      <Cta
+        appearance="secondary"
+        heading="Emphasis on organic certification of regional farms"
+        description="This call to cation should focus more on helping local / regional farms achieve and maintain a high standard w/ organic certification, with a link to find out more."
+        action={{
+          text: 'Learn more',
+          href: '/about',
+        }}
+      />
+      <ContentArea />
     </Fragment>
   );
 }
