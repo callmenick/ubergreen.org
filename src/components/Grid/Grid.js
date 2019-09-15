ef1eb911ff93ed0388dda134c4bf96b985a8bb0a
Nick Salloum <nick@callmenick.com>
2019-09-15 15:59:54 -0700

    adds bordered and gap prop styles

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import css from './Grid.css';
 
-function Grid({ children, padded, alignItems }) {
+function Grid({ children, gap, padded, bordered, alignItems }) {
   return (
     <div
       className={cx(css.grid, {
+        [css.gap]: gap,
         [css.padded]: padded,
+        [css.bordered]: bordered,
         [css[`alignItems-${alignItems}`]]: alignItems,
       })}
     >
@@ -18,12 +20,16 @@ function Grid({ children, padded, alignItems }) {
 
 Grid.propTypes = {
   children: PropTypes.node.isRequired,
+  gap: PropTypes.bool,
   padded: PropTypes.bool,
+  bordered: PropTypes.bool,
   alignItems: PropTypes.oneOf(['stretch', 'center', 'start', 'end']),
 };
 
 Grid.defaultProps = {
+  gap: true,
   padded: false,
+  bordered: false,
   alignItems: undefined,
 };
 
