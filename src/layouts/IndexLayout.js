7b11dd3e4615e828e86749f92f7f810368772b27
Nick Salloum <nick@callmenick.com>
2019-09-15 16:30:40 -0700

    adds favicon

--- src/layouts/IndexLayout.js
+++ src/layouts/IndexLayout.js
@@ -86,63 +86,12 @@ function IndexLayout({ children }) {
             href={`/_next/static/css/styles.chunk.css?v=${Date.now()}`}
           />
         )}
-        <link rel="shortcut icon" href="/static/favicon/favicon.png" />
-        <link
-          rel="icon"
-          type="image/x-icon"
-          sizes="57x57"
-          href="/static/favicon/favicon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/static/favicon/favicon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/static/favicon/favicon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/static/favicon/favicon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/static/favicon/favicon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/static/favicon/favicon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/static/favicon/favicon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/static/favicon/favicon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/static/favicon/favicon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/static/favicon/favicon-180x180.png"
-        />
+        <link rel="shortcut icon" href="/static/img/icons/favicon.png" />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/static/favicon/favicon-192x192.png"
+          href="/static/img/icons/favicon.png"
         />
       </Head>
       <Header />
