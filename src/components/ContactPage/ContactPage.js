bb124260d2a3d3c6d7baf60043b2c7d5a13ace5f
Nick Salloum <nick@callmenick.com>
2019-10-27 13:34:22 -0700

    inits contact page

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,80 +1,53 @@
 import React, { Fragment } from 'react';
-import PageHeader from 'components/PageHeader/PageHeader.js';
-import PageContent from 'components/PageContent/PageContent.js';
-import Form from 'components/Form/Form.js';
-import Input from 'components/Input/Input.js';
-import Button from 'components/Button/Button.js';
+import PageHeader from 'components/PageHeader/PageHeader';
+import PageContent, { Image } from 'components/PageContent/PageContent';
+import Button from 'components/Button/Button';
+import Instagram from 'components/Icons/Instagram';
+import css from './ContactPage.css';
 
 function ContactPage() {
   return (
     <Fragment>
       <PageHeader appearance="secondary" title="Get in touch" />
       <PageContent>
-        <Form>
-          <Form.Group>
-            <Input
-              type="text"
-              id="name"
-              name="name"
-              label="Name"
-              placeholder="Tell us your name (optional)"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Input
-              type="email"
-              id="email"
-              name="email"
-              label="Email"
-              placeholder="How can we reach you?"
-              required
-            />
-          </Form.Group>
-          <Form.Group>
-            <Input
-              type="textarea"
-              id="message"
-              name="message"
-              label="Message"
-              placeholder="Any additional info?"
-              required
-            />
-          </Form.Group>
-          <Form.Group>
+        <div className={css.info}>
+          <h3>
+            Let’s delight the world with
+            <br />
+            True Trinitaro Cacao!
+          </h3>
+          <p>
+            At Ubergreen Organics, we’re passionate about cacao – specifically,
+            we love sharing True Triniatario cacao with the world. Get in touch
+            with us to get a taste!
+          </p>
+          <ul>
+            <li>
+              <a href="mailto:info@ubergreen.org">info@ubergreen.org</a>
+            </li>
+            <li>
+              <a href="tel:+18683588725">+1 (868) 358-8725</a>
+            </li>
+          </ul>
+          <p>
+            Ubergreen Organics Limited
+            <br />
+            2D Fort Williams
+            <br />
+            St. James, Trindad & Tobago
+            <br />
+            West Indies
+          </p>
+          <p>
             <Button
-              theme={{
-                appearance: 'primary',
-                width: 'fill',
-                size: 'md',
-              }}
+              href="https://www.instagram.com/ubergreencocoa/"
+              target="_blank"
             >
-              Send
+              <Instagram fill="#00935B" />
             </Button>
-          </Form.Group>
-        </Form>
-        {/*
-        <form action="#">
-          <div>
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
-          </div>
-
-          <div>
-            <label htmlFor="question">What can we help you with?</label>
-            <select name="question" id="question">
-              <option>-- Choose one --</option>
-              <option value="foo">Foo</option>
-              <option value="bar">Bar</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" cols="30" rows="10" />
-          </div>
-        </form>
-        */}
+          </p>
+        </div>
+        <Image src="/static/img/about/pods.jpg" alt="bean" start={7} end={-2} />
       </PageContent>
     </Fragment>
   );
